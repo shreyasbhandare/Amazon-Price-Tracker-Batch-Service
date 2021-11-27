8122e7428a49c02e6117a42383cf90c2c898ba03
Shreyas Bhandare <bhandare.shreyas@gmail.com>
2021-11-26 22:48:17 -0500

    proxy removed

--- src/main/java/org/pricetrackerbatchapp/App.java
+++ src/main/java/org/pricetrackerbatchapp/App.java
@@ -3,24 +3,13 @@ package org.pricetrackerbatchapp;
 import jakarta.mail.MessagingException;
 import org.apache.commons.lang3.StringUtils;
 
-import java.net.Authenticator;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main( String[] args ) throws InterruptedException, MalformedURLException {
-        URL fixie = new URL(System.getenv("FIXIE_SOCKS_HOST"));
-        String[] fixieUserInfo = fixie.getUserInfo().split(":");
-        String fixieUser = fixieUserInfo[0];
-        String fixiePassword = fixieUserInfo[1];
-        System.setProperty("http.proxyHost", fixie.getHost());
-        System.setProperty("http.proxyPort", fixie.getPort() + "");
-        Authenticator.setDefault(new ProxyAuthenticator(fixieUser, fixiePassword));
-
+    public static void main( String[] args ) throws InterruptedException {
         doBatch();
     }
 
